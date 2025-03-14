d15edd3cf9d9caf2a4ca80640a9915b74a1e380c
uyava <liudmyla.kosianova@gmail.com>
2025-03-14 10:04:53 +0100

    class YearAvg: added constructor

--- src/yearavg.cpp
+++ src/yearavg.cpp
@@ -1,1 +1,6 @@
 #include "yearavg.h"
+
+YearAvg::YearAvg(double _temp, int _year, double _rain):temp(_temp),year(_year),rain(_rain)
+{
+    
+}
--- src/yearavg.h
+++ src/yearavg.h
@@ -1,3 +1,17 @@
 #pragma once
 
 #include <iostream>
+#include <sstream>
+#include <fstream>
+#include <algorithm>
+
+class YearAvg
+{
+    public:
+    //constructor
+    YearAvg(double _temp, int _year, double _rain);
+
+    private:
+    int year;
+    double temp, rain;
+};
