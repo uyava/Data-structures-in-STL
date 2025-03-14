58db3b1b5de3e8662ca73ee046d0307ef570f770
uyava <liudmyla.kosianova@gmail.com>
2025-03-14 09:36:09 +0100

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+//================================================
+//Sorts weather data and stores the data to a file
+//================================================
+
+#include "yearavg.h"
+
+int main()
+{
+
+    return 0;
+}
+
---
+++ src/yearavg.cpp
@@ -0,0 +1,1 @@
+#include "yearavg.h"
