caa6d6ad8340074d7a1202bc5716c010a2bdd7a5
uyava <liudmyla.kosianova@gmail.com>
2025-03-14 12:11:09 +0100

    class YearAvg: added setters and getters

--- src/yearavg.cpp
+++ src/yearavg.cpp
@@ -2,5 +2,34 @@
 
 YearAvg::YearAvg(double _temp, int _year, double _rain):temp(_temp),year(_year),rain(_rain)
 {
-    
+
+}
+
+void YearAvg::setTemp(double _temp)
+{
+    temp = _temp;
+}
+
+void YearAvg::setYear(int _year)
+{
+    year = _year;
+}
+
+void YearAvg::setRain(double _rain)
+{
+    rain = _rain;
+}
+
+double YearAvg::getTemp()
+{
+    return temp;
+}
+
+int YearAvg::getYear()
+{
+    return year;
+}
+double YearAvg::getRain()
+{
+    return rain;
 }
--- src/yearavg.h
+++ src/yearavg.h
@@ -11,6 +11,16 @@ class YearAvg
     //constructor
     YearAvg(double _temp, int _year, double _rain);
 
+    //"setters"
+    void setTemp(double _temp);
+    void setYear(int _year);
+    void setRain(double _rain);
+
+    //"getters"
+    double getTemp();
+    int getYear();
+    double getRain();
+
     private:
     int year;
     double temp, rain;
